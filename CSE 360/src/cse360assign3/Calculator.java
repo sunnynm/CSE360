7747fa7e7babcc8c9f108661af5154d439423822
sunnynm <snmahesh@asu.edu>
2016-02-22 14:07:00 -0700

    Initial Commit

---
+++ CSE 360/src/cse360assign3/Calculator.java
@@ -0,0 +1,41 @@
+package cse360assign3;
+
+/** Program made Calculator
+ * @author Sunny Mahesh PIN: 522
+ * 
+ * @version Feb 22, 2016
+ * 
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ CSE 360/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,47 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cse360assign2.OrderedIntList;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator alpha = new Calculator();
+		assertNotNull(alpha);
+	}
+
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
